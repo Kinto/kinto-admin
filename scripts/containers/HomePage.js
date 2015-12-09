1e5551d00ba1a266eb55da632068485a37ba0069
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-12-09 14:25:14 -0500

    Add a link to the Github project.

--- scripts/containers/HomePage.js
+++ scripts/containers/HomePage.js
@@ -5,6 +5,7 @@ export default class HomePage extends React.Component {
     return <div>
       <h1>Kinto Web Administration Console</h1>
       <p>Welcome.</p>
+      <p>You've noticed a bug? <a href="https://github.com/Kinto/kinto-admin/issues/new">File an issue</a>!</p>
     </div>;
   }
 }
