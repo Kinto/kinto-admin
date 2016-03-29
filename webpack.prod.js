623c4758301cf412d6da0c6a2fcfcf0ca16be0e1
Nicolas Perriault <nperriault@gmail.com>
2016-03-29 16:01:46 +0200

    Fixed generated bundle was missing bootstrap assets.

--- webpack.prod.js
+++ webpack.prod.js
@@ -43,8 +43,11 @@ module.exports = {
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract("css-loader"),
-        include: path.join(__dirname, "css")
-      }
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
   }
 };
