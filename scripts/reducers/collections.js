dc08b065983f6b7369b0fd3cac794687026dee86
Nicolas Perriault <nperriault@gmail.com>
2016-04-05 13:42:12 +0200

    Upgraded Babel to version 6.

--- scripts/reducers/collections.js
+++ scripts/reducers/collections.js
@@ -7,7 +7,7 @@ const INITIAL_STATE = {};
 
 export default function collections(state = INITIAL_STATE, action) {
   switch(action.type) {
-  case COLLECTION_SYNCED:
+  case COLLECTION_SYNCED: {
     // Update the synced status for the provided collection name
     const collectionState = {
       ...state[action.name],
@@ -17,6 +17,7 @@ export default function collections(state = INITIAL_STATE, action) {
       ...state,
       [action.name]: collectionState
     };
+  }
   case COLLECTIONS_LIST_RECEIVED:
     return action.collections;
   default:
