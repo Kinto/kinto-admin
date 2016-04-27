94d0f9209b32462f48d3a256964204f962a5720b
Mathieu Leplatre <mathieu@mozilla.com>
2016-04-27 18:02:44 +0200

    Support of schemas with definitions in bulk create

--- test/containers/BulkForm_test.js
+++ test/containers/BulkForm_test.js
@@ -18,60 +18,103 @@ import * as CollectionActions from "../../scripts/actions/collection";
 import jsonConfig from "../../config/config.json";
 
 
+function setupBulkForm(config) {
+  const props = {params: {name: "tasks"}};
+  const comp = setupContainer(<BulkFormPage {...props} />);
+  const { dispatch } = comp.store;
+  dispatch(CollectionsActions.collectionsListReceived(config));
+  dispatch(CollectionActions.select("tasks"));
+  return comp;
+}
+
+
 describe("BulkFormPage container", () => {
   var sandbox, comp;
 
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-    const props = {params: {name: "tasks"}};
-    comp = setupContainer(<BulkFormPage {...props} />);
-    const { dispatch } = comp.store;
-    dispatch(CollectionsActions.collectionsListReceived(jsonConfig.collections));
-    dispatch(CollectionActions.select("tasks"));
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
-
-  it("should render page title", () => {
-    expect(nodeText(comp, "h1")).eql("Bulk tasks creation");
-  });
-
-  it("should render a form", () => {
-    expect(nodeExists(comp, "form.rjsf")).eql(true);
-  });
-
-  it("should submit records", () => {
-    const create = sandbox.stub(KintoCollection.prototype, "create");
-
-    Simulate.click(findOne(comp, ".array-item-add button"));
-    Simulate.change(findAll(comp, "input[type=text]")[0], {
-      target: {value: "sampleTitle1"}
+  describe("Default config schema", () => {
+    beforeEach(() => {
+      sandbox = sinon.sandbox.create();
+      comp = setupBulkForm(jsonConfig.collections);
     });
 
-    return pause()
-      .then(() => {
-        Simulate.click(findOne(comp, ".array-item-add button"));
-        Simulate.change(findAll(comp, "input[type=text]")[1], {
-          target: {value: "sampleTitle2"}
-        });
-        return pause();
-      })
-      .then(() => {
-        Simulate.submit(findOne(comp, "form"));
+    afterEach(() => {
+      sandbox.restore();
+    });
 
-        sinon.assert.calledTwice(create);
-        sinon.assert.calledWith(create, {
-          done: false,
-          title: "sampleTitle1",
-          description: ""
-        });
-        sinon.assert.calledWith(create, {
-          done: false,
-          title: "sampleTitle2",
-          description: ""
-        });
+    it("should render page title", () => {
+      expect(nodeText(comp, "h1")).eql("Bulk tasks creation");
+    });
+
+    it("should render a form", () => {
+      expect(nodeExists(comp, "form.rjsf")).eql(true);
+    });
+
+    it("should submit records", () => {
+      const create = sandbox.stub(KintoCollection.prototype, "create");
+
+      Simulate.click(findOne(comp, ".array-item-add button"));
+      Simulate.change(findAll(comp, "input[type=text]")[0], {
+        target: {value: "sampleTitle1"}
       });
+
+      return pause()
+        .then(() => {
+          Simulate.click(findOne(comp, ".array-item-add button"));
+          Simulate.change(findAll(comp, "input[type=text]")[1], {
+            target: {value: "sampleTitle2"}
+          });
+          return pause();
+        })
+        .then(() => {
+          Simulate.submit(findOne(comp, "form"));
+
+          sinon.assert.calledTwice(create);
+          sinon.assert.calledWith(create, {
+            done: false,
+            title: "sampleTitle1",
+            description: ""
+          });
+          sinon.assert.calledWith(create, {
+            done: false,
+            title: "sampleTitle2",
+            description: ""
+          });
+        });
+    });
   });
+
+  describe("Custom schema with definitions", () => {
+    beforeEach(() => {
+      const config = {
+        collections: {
+          tasks: {
+            config: {
+              schema: {
+                definitions: {
+                  version: {
+                    type: "integer"
+                  }
+                },
+                type: "object",
+                properties: {
+                  version: {"$ref": "#/definitions/version"}
+                }
+              }
+            }
+          }
+        }
+      };
+      comp = setupBulkForm(config);
+    });
+
+    it("should support schema definitions references", () => {
+      Simulate.click(findOne(comp, ".array-item-add button"));
+      return pause()
+        .then(() => {
+          expect(nodeExists(comp, "form input")).eql(true);
+        });
+    });
+  });
+
+
 });
