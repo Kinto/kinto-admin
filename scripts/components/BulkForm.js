a40d8390d17dffc872c79ea79151151576be8d66
Nicolas Perriault <nperriault@gmail.com>
2016-05-16 15:07:11 +0200

    Bulk JSON record add form.

--- scripts/components/BulkForm.js
+++ scripts/components/BulkForm.js
@@ -3,30 +3,60 @@ import { Link } from "react-router";
 import Form from "react-jsonschema-form";
 
 import Spinner from "./Spinner";
+import JSONEditor from "./JSONEditor";
 
 
 export default class BulkForm extends Component {
   onSubmit = ({formData}) => {
-    const {params, notifyError, bulkCreateRecords} = this.props;
+    const {params, collection, notifyError, bulkCreateRecords} = this.props;
     const {bid, cid} = params;
-    if (formData.length > 0) {
-      bulkCreateRecords(bid, cid, formData);
-    } else {
-      notifyError({message: "The form is empty."});
+    const {schema} = collection;
+
+    if (formData.length === 0) {
+      return notifyError({message: "The form is empty."});
     }
+
+    if (Object.keys(schema).length === 0) {
+      console.log("yooo");
+      return bulkCreateRecords(bid, cid, formData.map(json => JSON.parse(json)));
+    }
+
+    bulkCreateRecords(bid, cid, formData);
   }
 
   render() {
     const {params, collection} = this.props;
     const {schema, uiSchema, busy} = collection;
     const {bid, cid} = params;
-    const bulkSchema = {
-      type: "array",
-      definitions: schema.definitions,
-      items: schema
-    };
-    const bulkUiSchema = {items: uiSchema};
-    const bulkFormData = [{}, {}];
+
+    let bulkSchema, bulkUiSchema, bulkFormData;
+
+    if (Object.keys(schema).length !== 0) {
+      bulkSchema = {
+        type: "array",
+        definitions: schema.definitions,
+        items: schema
+      };
+      bulkUiSchema = {
+        items: uiSchema
+      };
+      bulkFormData = [{}, {}];
+    } else {
+      bulkSchema = {
+        type: "array",
+        items: {
+          type: "string",
+          default: "{}"
+        }
+      };
+      bulkUiSchema = {
+        items: {
+          "ui:widget": JSONEditor
+        }
+      };
+      bulkFormData = ["{}", "{}"];
+    }
+
     return (
       <div>
         <h1>Bulk {bid}/{cid} creation</h1>
