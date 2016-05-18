605177117e26c5e38e14e3cf619f551b8e9a3b61
Nicolas Perriault <nperriault@gmail.com>
2016-05-18 16:20:30 +0200

    AddForm component tests.

---
+++ test/components/AddForm_test.js
@@ -0,0 +1,62 @@
+import { expect } from "chai";
+import sinon from "sinon";
+import { Simulate } from "react-addons-test-utils";
+
+import { createSandbox, createComponent } from "../test_utils";
+
+import AddForm from "../../scripts/components/AddForm";
+
+
+describe("AddForm component", () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe("Schema defined", () => {
+    let node, createRecord;
+    const collection = {
+      schema: {
+        type: "object",
+        properties: {
+          foo: {
+            type: "string"
+          }
+        }
+      }
+    };
+
+    beforeEach(() => {
+      createRecord = sinon.spy();
+      node = createComponent(AddForm, {
+        params: {bid: "bucket", cid: "collection"},
+        collection,
+        createRecord,
+      });
+    });
+
+    it("should render a form", () => {
+      expect(node.querySelector("form")).to.exist;
+    });
+
+    it("should submitted entered data", () => {
+      Simulate.change(node.querySelector("#root_foo"), {
+        target: {value: "bar"}
+      });
+
+      Simulate.submit(node.querySelector("form"));
+
+      sinon.assert.calledWithExactly(
+        createRecord, "bucket", "collection", {foo: "bar"});
+    });
+  });
+
+  describe.skip("No schema defined", () => {
+    // XXX CodeMirror seems to be totally incompatible with JSDom.
+  });
+});
