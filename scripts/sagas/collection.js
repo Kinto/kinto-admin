ba68e3970e44255ab35876e235a1b10c8eea41b8
Nicolas Perriault <nperriault@gmail.com>
2016-05-22 17:19:58 +0200

    Dispatch listRecords actions instead of blocking.

--- scripts/sagas/collection.js
+++ scripts/sagas/collection.js
@@ -11,7 +11,7 @@ import {
 } from "../constants";
 import { getClient } from "../client";
 import { notifySuccess, notifyError } from "../actions/notifications";
-import { collectionBusy, listRecordsSuccess } from "../actions/collection";
+import * as collectionActions from "../actions/collection";
 import { recordLoadSuccess, resetRecord } from "../actions/record";
 
 
@@ -26,13 +26,13 @@ function getCollection(bid, cid) {
 export function* listRecords(bid, cid) {
   const coll = getCollection(bid, cid);
   try {
-    yield put(collectionBusy(true));
+    yield put(collectionActions.collectionBusy(true));
     const {data} = yield call([coll, coll.listRecords]);
-    yield put(listRecordsSuccess(data));
+    yield put(collectionActions.listRecordsSuccess(data));
   } catch(error) {
     yield put(notifyError(error));
   } finally {
-    yield put(collectionBusy(false));
+    yield put(collectionActions.collectionBusy(false));
   }
 }
 
@@ -52,7 +52,7 @@ export function* createRecord(bid, cid, record) {
   // XXX mark collection and record as busy
   try {
     yield call([coll, coll.createRecord], record);
-    yield listRecords(bid, cid); // XXX put action instead
+    yield put(collectionActions.listRecords(bid, cid));
     yield put(updatePath(`/buckets/${bid}/collections/${cid}`));
     yield put(notifySuccess("Record added."));
   } catch(error) {
@@ -66,7 +66,7 @@ export function* updateRecord(bid, cid, rid, record) {
   try {
     yield call([coll, coll.updateRecord], {...record, id: rid});
     yield put(resetRecord());
-    yield listRecords(bid, cid); // XXX put action instead
+    yield put(collectionActions.listRecords(bid, cid));
     yield put(updatePath(`/buckets/${bid}/collections/${cid}`));
     yield put(notifySuccess("Record added."));
   } catch(error) {
@@ -79,7 +79,7 @@ export function* deleteRecord(bid, cid, rid) {
   // XXX mark collection and record as busy
   try {
     yield call([coll, coll.deleteRecord], rid);
-    yield listRecords(bid, cid); // XXX put action instead
+    yield put(collectionActions.listRecords(bid, cid));
     yield put(updatePath(`/buckets/${bid}/collections/${cid}`));
     yield put(notifySuccess("Record deleted."));
   } catch(error) {
@@ -101,7 +101,7 @@ export function* bulkCreateRecords(bid, cid, records) {
       err.details = errors.map(err => err.error.message);
       throw err;
     } else {
-      yield listRecords(bid, cid); // XXX dispatch action instead
+      yield put(collectionActions.listRecords(bid, cid));
       yield put(updatePath(`/buckets/${bid}/collections/${cid}`));
       yield put(notifySuccess(`${published.length} records created.`));
     }
