000873969e04db0593db4ebe4eb75a62922b5876
Nicolas Perriault <nperriault@gmail.com>
2016-05-25 14:37:34 +0200

    Fixes #85: Collection deletion confirmation prompt. r=@Natim (#87)

--- scripts/components/CollectionEdit.js
+++ scripts/components/CollectionEdit.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import Form from "react-jsonschema-form";
 
 import CollectionForm from "./CollectionForm";
 import Spinner from "./Spinner";
 
+
+const deleteSchema = {
+  type: "string",
+  title: "Please enter the collection name to delete as a confirmation",
+};
+
+function DeleteForm({cid, onSubmit}) {
+  const validate = (formData, errors) => {
+    if (formData !== cid) {
+      errors.addError("The collection name does not match.");
+    }
+    return errors;
+  };
+  return (
+    <Form
+      schema={deleteSchema}
+      validate={validate}
+      onSubmit={({formData}) => onSubmit(formData)}>
+      <button type="submit" className="btn btn-danger">Delete collection</button>
+    </Form>
+  );
+}
 
 export default class CollectionEdit extends Component {
   onSubmit = (formData) => {
@@ -11,16 +34,17 @@ export default class CollectionEdit extends Component {
     this.props.updateCollection(bid, cid, formData);
   };
 
-  onDeleteClick = () => {
+  deleteCollection = (cid) => {
     const {deleteCollection, params} = this.props;
-    const {bid, cid} = params;
+    const {bid} = params;
     if (confirm("This will delete the collection and all the records it contains. Are you sure?")) {
       deleteCollection(bid, cid);
     }
   };
 
   render() {
-    const {collection} = this.props;
+    const {params, collection} = this.props;
+    const {cid} = params;
     const {schema={}, uiSchema={}, displayFields=[], label, busy} = collection;
     if (busy) {
       return <Spinner />;
@@ -45,10 +69,9 @@ export default class CollectionEdit extends Component {
           </div>
           <div className="panel-body">
             <p>
-              Delete the collection and all the records it contains.
+              Delete the <b>{label}</b> collection and all the records it contains.
             </p>
-            <button className="btn btn-danger"
-              onClick={this.onDeleteClick}>Delete collection</button>
+            <DeleteForm cid={cid} onSubmit={this.deleteCollection} />
           </div>
         </div>
       </div>
