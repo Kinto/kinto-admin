a00503a9bf58b0fc40fc1280f12d94d1cc55e871
Nicolas Perriault <nperriault@gmail.com>
2016-05-30 12:01:40 +0200

    Added tests for route sagas.

--- scripts/sagas/route.js
+++ scripts/sagas/route.js
@@ -1,32 +1,56 @@
-import { take, fork, put } from "redux-saga/effects";
+import { call, take, fork, put } from "redux-saga/effects";
 
 import { SESSION_SETUP_COMPLETE, ROUTE_LOAD_REQUEST } from "../constants";
 import { getClient } from "../client";
-import { resetBucket, bucketLoadSuccess } from "../actions/bucket";
-import { resetCollection, collectionLoadSuccess } from "../actions/collection";
+import { resetBucket, bucketBusy, bucketLoadSuccess } from "../actions/bucket";
+import { resetCollection, collectionBusy, collectionLoadSuccess } from "../actions/collection";
 import { resetRecord, recordLoadSuccess } from "../actions/record";
 import { notifyError } from "../actions/notifications";
 
 
-export function* loadRoute(bid, cid, rid) {
-  try {
-    // yield put(resetBucket());
-    // yield put(resetCollection());
-    // yield put(resetRecord());
-    const res = yield getClient().batch(batch => {
-      if (bid) {
-        const bucket = batch.bucket(bid);
-        bucket.getData();
-        if (cid) {
-          const coll = bucket.collection(cid);
-          coll.getData();
-          if (rid) {
-            coll.getRecord(rid);
-          }
+function getBatchLoadFn(bid, cid, rid) {
+  return (batch) => {
+    if (bid) {
+      const bucket = batch.bucket(bid);
+      bucket.getData();
+      if (cid) {
+        const coll = bucket.collection(cid);
+        coll.getData();
+        if (rid) {
+          coll.getRecord(rid);
         }
       }
-    });
-    const [bucket, collection, record] = res.map(({status, body}) => {
+    }
+  };
+}
+
+export function* loadRoute(bid, cid, rid) {
+  // If we don't have anything to load, exit
+  if (!bid && !cid && !rid) {
+    return;
+  }
+
+  try {
+    const client = getClient();
+
+    // Reset all currently selected resources
+    yield put(resetBucket());
+    yield put(resetCollection());
+    yield put(resetRecord());
+
+    // Mark bucket and collection as busy if we are loading them
+    if (bid) {
+      yield put(bucketBusy(true));
+    }
+
+    if (cid) {
+      yield put(collectionBusy(true));
+    }
+    // Fetch all currently selected resource data in a single batch request
+    const res = yield call([client, client.batch], getBatchLoadFn(bid, cid, rid));
+
+    // Map them to state
+    const [bucket, collection, record] = res.map(({body}) => {
       return body.data;
     });
     if (bucket) {
@@ -40,11 +64,16 @@ export function* loadRoute(bid, cid, rid) {
     }
   } catch(error) {
     yield put(notifyError(error));
+  } finally {
+    yield put(bucketBusy(false));
+    yield put(collectionBusy(false));
   }
 }
 
 export function* watchLoadRoute() {
+  // Ensures the session setup is already completed
   yield take(SESSION_SETUP_COMPLETE);
+
   while(true) { // eslint-disable-line
     const {bid, cid, rid} = yield take(ROUTE_LOAD_REQUEST);
     yield fork(loadRoute, bid, cid, rid);
