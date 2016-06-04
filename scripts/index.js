848757f36555d60a4efcfc7af1065823193ab4a2
Nicolas Perriault <nperriault@gmail.com>
2016-06-04 20:16:11 +0200

    Got rid of react-router query string parameters.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { Router } from "react-router";
+import { Router, useRouterHistory } from "react-router";
 import { syncHistoryWithStore, push as updatePath } from "react-router-redux";
-import { hashHistory } from "react-router";
+import { createHashHistory } from "history";
 import getRoutes from "./routes";
 import configureStore from "./store/configureStore";
 import * as routeActions from "./actions/route";
@@ -16,6 +16,9 @@ import "../css/styles.css";
 
 const store = configureStore();
 
+// Get rid of react-router query string parameter
+// https://github.com/reactjs/react-router/issues/1967#issuecomment-214659714
+const hashHistory = useRouterHistory(createHashHistory)({queryKey: false});
 syncHistoryWithStore(hashHistory, store);
 
 function onRouteUpdate() {
