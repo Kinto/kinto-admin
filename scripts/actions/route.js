c6941d4963aa83fe7c10ca790594fff66f295da7
Nicolas Perriault <nperriault@gmail.com>
2016-06-08 18:27:43 +0200

    Route action types.

--- scripts/actions/route.js
+++ scripts/actions/route.js
@@ -1,13 +1,25 @@
+/* @flow */
+
+import type { Action, Bucket, Collection, Record } from "../types";
+
 import {
   ROUTE_LOAD_SUCCESS,
   ROUTE_UPDATED,
 } from "../constants";
 
 
-export function routeLoadSuccess(bucket, collection, record) {
+export function routeLoadSuccess(
+  bucket: Bucket,
+  collection: Collection,
+  record: Record
+): Action {
   return {type: ROUTE_LOAD_SUCCESS, bucket, collection, record};
 }
 
-export function routeUpdated(authenticated, params, location) {
+export function routeUpdated(
+  authenticated: boolean,
+  params: Object,
+  location: Object
+): Action {
   return {type: ROUTE_UPDATED, authenticated, params, location};
 }
