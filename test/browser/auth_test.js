20e5b20097abe353ac8823b6f6bcce4f8dcc8822
Nicolas Perriault <nperriault@gmail.com>
2016-06-12 09:57:17 +0200

    WiP: browser tests.

---
+++ test/browser/auth_test.js
@@ -0,0 +1,45 @@
+import { install as installGeneratorSupport } from "mocha-generators";
+import Nightmare from "nightmare";
+import { expect } from "chai";
+
+import {
+  start as startStaticTestServer,
+  stop as stopStaticTestServer,
+} from "../../testServer";
+
+installGeneratorSupport();
+
+
+describe("Auth tests", function() {
+  this.timeout(10000);
+
+  let browser;
+
+  beforeEach(() => {
+    browser = Nightmare({show: false});
+    return startStaticTestServer();
+  });
+
+  afterEach(() => {
+    return stopStaticTestServer();
+  });
+
+  it("should open the homepage", function* () {
+    const result = yield browser
+      .goto("http://localhost:3000/")
+      .wait(".rjsf")
+      .type("#root_credentials_username", "")
+      .type("#root_credentials_username", "__test__")
+      .type("#root_credentials_password", "")
+      .type("#root_credentials_password", "__test__")
+      .click(".rjsf button.btn.btn-info")
+      .wait(".session-info-bar")
+      .evaluate(() => {
+        return document.querySelector(".session-info-bar strong").textContent;
+      })
+      .end();
+
+    expect(result).eql("__test__");
+  });
+});
+
---
+++ testServer.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var express = require("express");
+
+var app = express();
+app.use(express.static(path.join(__dirname, "dist")));
+
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "index.dev.html"));
+// });
+
+let server;
+
+function start() {
+  return new Promise((resolve, reject) => {
+    server = app.listen(3000, "0.0.0.0", function(err) {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve();
+    });
+  });
+}
+
+function stop() {
+  return new Promise((resolve) => {
+    server.close(resolve);
+  });
+}
+
+module.exports = {start, stop};
