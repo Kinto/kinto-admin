2a6649dae3ef051d0bfd40ff136eae95847826cd
Nicolas Perriault <nperriault@gmail.com>
2016-06-27 14:38:29 +0200

    Expose constants as a module obj in root saga.

--- scripts/sagas/index.js
+++ scripts/sagas/index.js
@@ -1,24 +1,7 @@
 import { takeEvery } from "redux-saga";
 import { fork } from "redux-saga/effects";
 
-import {
-  ROUTE_UPDATED,
-  SESSION_SETUP,
-  SESSION_SETUP_COMPLETE,
-  SESSION_BUCKETS_REQUEST,
-  SESSION_LOGOUT,
-  BUCKET_CREATE_REQUEST,
-  BUCKET_UPDATE_REQUEST,
-  BUCKET_DELETE_REQUEST,
-  COLLECTION_CREATE_REQUEST,
-  COLLECTION_UPDATE_REQUEST,
-  COLLECTION_DELETE_REQUEST,
-  COLLECTION_RECORDS_REQUEST,
-  RECORD_CREATE_REQUEST,
-  RECORD_UPDATE_REQUEST,
-  RECORD_DELETE_REQUEST,
-  ATTACHMENT_DELETE_REQUEST,
-} from "../constants";
+import * as c from "../constants";
 import * as sessionSagas from "./session";
 import * as routeSagas from "./route";
 import * as bucketSagas from "./bucket";
@@ -31,26 +14,26 @@ import * as collectionSagas from "./collection";
 export default function* rootSaga(getState) {
   yield [
     // session
-    takeEvery(SESSION_SETUP, sessionSagas.setupSession, getState),
-    takeEvery(SESSION_SETUP_COMPLETE, sessionSagas.handleSessionRedirect, getState),
-    takeEvery(SESSION_BUCKETS_REQUEST, sessionSagas.listBuckets, getState),
-    takeEvery(SESSION_LOGOUT, sessionSagas.sessionLogout, getState),
+    takeEvery(c.SESSION_SETUP, sessionSagas.setupSession, getState),
+    takeEvery(c.SESSION_SETUP_COMPLETE, sessionSagas.handleSessionRedirect, getState),
+    takeEvery(c.SESSION_BUCKETS_REQUEST, sessionSagas.listBuckets, getState),
+    takeEvery(c.SESSION_LOGOUT, sessionSagas.sessionLogout, getState),
     // route
-    takeEvery(ROUTE_UPDATED, routeSagas.routeUpdated, getState),
+    takeEvery(c.ROUTE_UPDATED, routeSagas.routeUpdated, getState),
     // bucket/collections
-    takeEvery(BUCKET_CREATE_REQUEST, bucketSagas.createBucket, getState),
-    takeEvery(BUCKET_UPDATE_REQUEST, bucketSagas.updateBucket, getState),
-    takeEvery(BUCKET_DELETE_REQUEST, bucketSagas.deleteBucket, getState),
-    takeEvery(COLLECTION_CREATE_REQUEST, bucketSagas.createCollection, getState),
-    takeEvery(COLLECTION_UPDATE_REQUEST, bucketSagas.updateCollection, getState),
-    takeEvery(COLLECTION_DELETE_REQUEST, bucketSagas.deleteCollection, getState),
+    takeEvery(c.BUCKET_CREATE_REQUEST, bucketSagas.createBucket, getState),
+    takeEvery(c.BUCKET_UPDATE_REQUEST, bucketSagas.updateBucket, getState),
+    takeEvery(c.BUCKET_DELETE_REQUEST, bucketSagas.deleteBucket, getState),
+    takeEvery(c.COLLECTION_CREATE_REQUEST, bucketSagas.createCollection, getState),
+    takeEvery(c.COLLECTION_UPDATE_REQUEST, bucketSagas.updateCollection, getState),
+    takeEvery(c.COLLECTION_DELETE_REQUEST, bucketSagas.deleteCollection, getState),
     // collection/records
-    takeEvery(COLLECTION_RECORDS_REQUEST, collectionSagas.listRecords, getState),
-    takeEvery(RECORD_CREATE_REQUEST, collectionSagas.createRecord, getState),
-    takeEvery(RECORD_UPDATE_REQUEST, collectionSagas.updateRecord, getState),
-    takeEvery(RECORD_DELETE_REQUEST, collectionSagas.deleteRecord, getState),
+    takeEvery(c.COLLECTION_RECORDS_REQUEST, collectionSagas.listRecords, getState),
+    takeEvery(c.RECORD_CREATE_REQUEST, collectionSagas.createRecord, getState),
+    takeEvery(c.RECORD_UPDATE_REQUEST, collectionSagas.updateRecord, getState),
+    takeEvery(c.RECORD_DELETE_REQUEST, collectionSagas.deleteRecord, getState),
     fork(collectionSagas.watchBulkCreateRecords),
     // attachments
-    takeEvery(ATTACHMENT_DELETE_REQUEST, collectionSagas.deleteAttachment, getState),
+    takeEvery(c.ATTACHMENT_DELETE_REQUEST, collectionSagas.deleteAttachment, getState),
   ];
 }
