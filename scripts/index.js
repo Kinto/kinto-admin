9d43432575073d577b1b4cac0dc94d34bdf5af09
Nicolas Perriault <nperriault@gmail.com>
2016-07-07 18:04:53 +0200

    Enable loading custom plugins for dev.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,15 @@
 import renderAdmin from "./app";
-import * as testPlugin from "../sample/plugin";
 
+// Check for local plugins to enable for development.
+const plugins = (process.env.KINTO_ADMIN_PLUGINS || "")
+  .split(",")
+  .filter(x => !!x)
+  .map(pluginName => {
+    try {
+      return require(`../plugins/${pluginName}/index`);
+    } catch(err) {
+      console.warn(`Couldn't load plugin ${pluginName}: ${err}`);
+    }
+  });
 
-renderAdmin(document.getElementById("app"), [testPlugin]);
+renderAdmin(document.getElementById("app"), plugins);
--- webpack.prod.js
+++ webpack.prod.js
@@ -19,7 +19,8 @@ module.exports = {
     new ExtractTextPlugin("styles.css", {allChunks: true}),
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production")
+        NODE_ENV: JSON.stringify("production"),
+        KINTO_ADMIN_PLUGINS: JSON.stringify(process.env.KINTO_ADMIN_PLUGINS),
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
