f1157829b655fa461ff01c972a1c549f2b2671f8
Nicolas Perriault <nperriault@gmail.com>
2016-08-24 16:34:58 +0200

    Improve tab styling.

--- scripts/components/CollectionTabs.js
+++ scripts/components/CollectionTabs.js
@@ -10,14 +10,23 @@ export default class CollectionTabs extends Component {
       <div className="tabs-container">
         <ul className="nav nav-tabs nav-justified">
           <li role="presentation" className={selected === "records" ? "active" : ""}>
-            <Link to={`/buckets/${bid}/collections/${cid}`}>Records</Link>
+            <Link to={`/buckets/${bid}/collections/${cid}`}>
+              <i className="glyphicon glyphicon-align-justify" />
+              Records
+            </Link>
           </li>
           {"history" in capabilities ?
             <li role="presentation" className={selected === "history" ? "active" : ""}>
-              <Link to={`/buckets/${bid}/collections/${cid}/history`}>History</Link>
+              <Link to={`/buckets/${bid}/collections/${cid}/history`}>
+                <i className="glyphicon glyphicon-time" />
+                History
+              </Link>
             </li> : null}
           <li role="presentation" className={selected === "settings" ? "active" : ""}>
-            <Link to={`/buckets/${bid}/collections/${cid}/edit`}>Settings</Link>
+            <Link to={`/buckets/${bid}/collections/${cid}/edit`}>
+              <i className="glyphicon glyphicon-cog" />
+              Properties
+            </Link>
           </li>
         </ul>
         <div className="panel panel-default">
