a5f73c20241c670ed5bab8dc22410952c2a53ed1
Nicolas Perriault <nperriault@gmail.com>
2016-08-26 16:19:00 +0200

    Added bucket reducer tests.

--- test/reducers/bucket_test.js
+++ test/reducers/bucket_test.js
@@ -5,6 +5,12 @@ import {
   BUCKET_BUSY,
   BUCKET_RESET,
   BUCKET_LOAD_SUCCESS,
+  BUCKET_COLLECTIONS_REQUEST,
+  BUCKET_COLLECTIONS_SUCCESS,
+  BUCKET_GROUPS_REQUEST,
+  BUCKET_GROUPS_SUCCESS,
+  BUCKET_HISTORY_REQUEST,
+  BUCKET_HISTORY_SUCCESS,
 } from "../../scripts/constants";
 
 
@@ -44,5 +50,77 @@ describe("bucket reducer", () => {
         read: ["github:n1k0", "github:almet"],
       }
     });
+  });
+
+  it("BUCKET_COLLECTIONS_REQUEST", () => {
+    const state = {collectionsLoaded: true};
+    const action = {type: BUCKET_COLLECTIONS_REQUEST};
+
+    expect(bucket(state, action))
+      .to.have.property("collectionsLoaded").eql(false);
+  });
+
+  it("BUCKET_COLLECTIONS_SUCCESS", () => {
+    const state = {
+      collections: [],
+      collectionsLoaded: false,
+    };
+    const action = {
+      type: BUCKET_COLLECTIONS_SUCCESS,
+      collections: [1, 2, 3],
+    };
+
+    expect(bucket(state, action))
+      .to.have.property("collections").eql(action.collections);
+    expect(bucket(state, action))
+      .to.have.property("collectionsLoaded").eql(true);
+  });
+
+  it("BUCKET_GROUPS_REQUEST", () => {
+    const state = {groupsLoaded: true};
+    const action = {type: BUCKET_GROUPS_REQUEST};
+
+    expect(bucket(state, action))
+      .to.have.property("groupsLoaded").eql(false);
+  });
+
+  it("BUCKET_GROUPS_SUCCESS", () => {
+    const state = {
+      groups: [],
+      groupsLoaded: false,
+    };
+    const action = {
+      type: BUCKET_GROUPS_SUCCESS,
+      groups: [1, 2, 3],
+    };
+
+    expect(bucket(state, action))
+      .to.have.property("groups").eql(action.groups);
+    expect(bucket(state, action))
+      .to.have.property("groupsLoaded").eql(true);
+  });
+
+  it("BUCKET_HISTORY_REQUEST", () => {
+    const state = {historyLoaded: true};
+    const action = {type: BUCKET_HISTORY_REQUEST};
+
+    expect(bucket(state, action))
+      .to.have.property("historyLoaded").eql(false);
+  });
+
+  it("BUCKET_HISTORY_SUCCESS", () => {
+    const state = {
+      history: [],
+      historyLoaded: false,
+    };
+    const action = {
+      type: BUCKET_HISTORY_SUCCESS,
+      history: [1, 2, 3],
+    };
+
+    expect(bucket(state, action))
+      .to.have.property("history").eql(action.history);
+    expect(bucket(state, action))
+      .to.have.property("historyLoaded").eql(true);
   });
 });
