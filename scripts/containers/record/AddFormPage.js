3ede4f8cfb57d6e8b02f0cd1fa9372181f5396c7
Nicolas Perriault <nperriault@gmail.com>
2016-08-31 14:07:41 +0200

    Move record containers to their own directory.

--- scripts/containers/record/AddFormPage.js
+++ scripts/containers/record/AddFormPage.js
@@ -2,15 +2,16 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { push as updatePath } from "react-router-redux";
 
-import BulkForm from "../components/record/BulkForm";
-import * as CollectionActions from "../actions/collection";
-import * as NotificationsActions from "../actions/notifications";
+import AddForm from "../../components/record/AddForm";
+import * as CollectionActions from "../../actions/collection";
+import * as NotificationsActions from "../../actions/notifications";
 
 
 function mapStateToProps(state) {
   return {
-    collection: state.collection,
     session: state.session,
+    collection: state.collection,
+    record: state.record,
   };
 }
 
@@ -25,4 +26,4 @@ function mapDispatchToProps(dispatch) {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(BulkForm);
+)(AddForm);
