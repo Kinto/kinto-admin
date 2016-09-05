e81655436cf091362cfdb791b941659cb77d89ef
Nicolas Perriault <nicolas@perriault.net>
2016-09-05 11:52:32 +0200

    Removed CLI executable, simple plugin loading.

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 
 import KintoAdmin from "./app";
+import * as signoffPlugin from "../plugins/signoff";
 
 
-// Check for local plugins to enable for development.
-const plugins = (process.env.KINTO_ADMIN_PLUGINS || "")
-  .split(",")
-  .filter(x => !!x)
-  .map(pluginName => {
-    try {
-      // XXX: For now we only support local core plugins
-      if (pluginName === "signoff") {
-        return require("../plugins/signoff/index");
-      } else if (pluginName === "example") {
-        return require("../plugins/example/index");
-      }
-      throw `Unknown plugin ${pluginName}`;
-    } catch(err) {
-      console.warn(`Couldn't load plugin ${pluginName}: ${err}`);
-    }
-  });
+const corePlugins = [
+  signoffPlugin
+];
 
-render(<KintoAdmin plugins={plugins} />, document.getElementById("app"));
+render(<KintoAdmin plugins={corePlugins} />, document.getElementById("app"));
