4e247e36961f3a78cac81238bb3157d1bac8bbd5
Mathieu Leplatre <mathieu@mozilla.com>
2016-10-05 16:41:47 +0200

    Button for to-review/to-sign

--- src/plugins/signoff/index.js
+++ src/plugins/signoff/index.js
@@ -1,34 +1,65 @@
 import React from "react";
 import { takeEvery } from "redux-saga";
 import { call, put } from "redux-saga/effects";
+import { push as updatePath } from "react-router-redux";
 
 import { getClient } from "../../client";
 import { notifySuccess, notifyError } from "../../actions/notifications";
 
 
+const PLUGIN_REVIEW_REQUEST = "PLUGIN_REVIEW_REQUEST";
+const PLUGIN_DECLINE_REQUEST = "PLUGIN_DECLINE_REQUEST";
 const PLUGIN_SIGNOFF_REQUEST = "PLUGIN_SIGNOFF_REQUEST";
 
 // Actions
-function requestSignoff() {
+function requestReview() {
+  return {type: PLUGIN_REVIEW_REQUEST};
+}
+function declineChanges() {
+  return {type: PLUGIN_DECLINE_REQUEST};
+}
+function approveChanges() {
   return {type: PLUGIN_SIGNOFF_REQUEST};
 }
 
 
 function* handleSignoffRequest(getState, action) {
   // Obtain current bucket and collection ids from state.
-  const {collection:collectionState} = getState();
-  const {bucket: bid, name: cid} = collectionState;
+  const {bucket: bucketState, collection: collectionState} = getState();
+  const {type} = action;
+  const {id: bid} = bucketState;
+  const {id: cid} = collectionState;
 
-  // XXX: Currently the signoff feature does not exist on the server.
-  // Meanwhile we will just trigger a signature.
+  let status;
+  let message;
+  switch (type) {
+    case PLUGIN_SIGNOFF_REQUEST:
+      status = {status: "to-sign"};
+      message = "Signature requested.";
+      break;
+    case PLUGIN_DECLINE_REQUEST:
+      status = {status: "work-in-progress"};
+      message = "Changes declined.";
+      break;
+    case PLUGIN_REVIEW_REQUEST:
+      status = {status: "to-review"};
+      message = "Review requested.";
+      break;
+  }
+
   const client = getClient();
-  // Set "status" metadata to trigger the signature.
   const coll = client.bucket(bid).collection(cid);
+  const {last_modified} = collectionState.data;
   try {
-    yield call([coll, coll.setData], {status: "to-sign"}, {patch: true});
-    yield put(notifySuccess("Signature requested."));
+    yield call([coll, coll.setData], status, {
+      safe: true,
+      patch: true,
+      last_modified
+    });
+    yield put(updatePath(`/buckets/${bid}/collections/${cid}/records`));
+    yield put(notifySuccess(message));
   } catch (e) {
-    yield put(notifyError("Couldn't sign collection.", e));
+    yield put(notifyError("Couldn't change collection status.", e));
   }
 }
 
@@ -53,20 +84,42 @@ class SignoffButton extends React.Component {
       return null;
     }
 
+    const {status} = collectionState.data;
+    let action;
+    let label;
+    switch(status) {
+      case "work-in-progress":
+        action = requestReview();
+        label = "Request review";
+        break;
+      case "to-review":
+        action = approveChanges();
+        label = "Approve changes";
+        break;
+      case "signed":
+        action = approveChanges();
+        label = "Re-sign";
+        break;
+      // XXX: empty ?
+      // XXX: decline: to-review -> work-in-progress
+    }
+
     return (
       <a className="btn btn-info"
          href="#"
          onClick={(event) => {
            event.preventDefault();
-           dispatch(requestSignoff());
-         }}>Request signoff</a>
+           dispatch(action);
+         }}>{label}</a>
     );
   }
 }
 
 
 export const sagas = [
-  [takeEvery, PLUGIN_SIGNOFF_REQUEST, handleSignoffRequest]
+  [takeEvery, PLUGIN_REVIEW_REQUEST, handleSignoffRequest],
+  [takeEvery, PLUGIN_DECLINE_REQUEST, handleSignoffRequest],
+  [takeEvery, PLUGIN_SIGNOFF_REQUEST, handleSignoffRequest],
 ];
 
 export const reducers = {};
