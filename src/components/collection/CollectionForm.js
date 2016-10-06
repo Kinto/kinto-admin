a622b3ab9fd490febdcc0b169ae1f08d68a040c3
Mathieu Leplatre <mathieu@leplat.re>
2016-10-06 15:09:27 +0200

    Fix #117: Improve default json schema (#264)

--- src/components/collection/CollectionForm.js
+++ src/components/collection/CollectionForm.js
@@ -10,14 +10,14 @@ import { validateSchema, validateUiSchema } from "../../utils";
 const defaultSchema = JSON.stringify({
   type: "object",
   properties: {
-    field1: {type: "string"},
-    field2: {type: "string"},
+    title: {type: "string", title: "Title", description: "Short title"},
+    content: {type: "string", title: "Content", description: "Provide details..."},
   }
 }, null, 2);
 
 const defaultUiSchema = JSON.stringify({
-  "ui:order": ["field1", "field2"],
-  field2: {
+  "ui:order": ["title", "content"],
+  content: {
     "ui:widget": "textarea"
   }
 }, null, 2);
@@ -118,7 +118,7 @@ const schema = {
           property names of your records.</em>
         </p>
       ),
-      default: ["field1", "field2"],
+      default: ["title"],
       minItems: 1,
       items: {
         type: "string",
