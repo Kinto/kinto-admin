ca797c3aaba1337cc0965e4718e110300a033511
Mathieu Leplatre <mathieu@mozilla.com>
2016-10-06 12:56:30 +0200

    Add link to history entries (fixes #209)

--- src/components/HistoryTable.js
+++ src/components/HistoryTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router";
 
 
 class HistoryRow extends Component {
@@ -14,21 +15,34 @@ class HistoryRow extends Component {
 
   render() {
     const {open} = this.state;
-    const {entry} = this.props;
+    const {entry, bid} = this.props;
     const {
       date,
       action,
       resource_name,
       target,
       user_id,
+      collection_id: cid,
+      group_id: gid,
+      record_id: rid
     } = entry;
+
+    const objectId = target.data.id;
+
+    const link = {
+      bucket: `/buckets/${bid}/edit`,
+      collection: `/buckets/${bid}/collections/${cid}/edit`,
+      group: `/buckets/${bid}/groups/${gid}/edit`,
+      record: `/buckets/${bid}/collections/${cid}/edit/${rid}` // XXX: see Kinto/kinto-admin#204
+    }[resource_name];
+
     return (
       <tbody>
         <tr>
           <td>{date}</td>
           <td>{action}</td>
           <td>{resource_name}</td>
-          <td>{target && target.data && target.data.id}</td>
+          <td>{link ? <Link to={link}>{objectId}</Link> : objectId}</td>
           <td>{user_id}</td>
           <td className="text-center">
             <a href="." className="btn btn-xs btn-default"
@@ -51,7 +65,7 @@ class HistoryRow extends Component {
 
 export default class HistoryTable extends Component {
   render() {
-    const {history} = this.props;
+    const {history, bid} = this.props;
     return (
       <table className="table table-striped table-bordered record-list">
         <thead>
@@ -66,7 +80,7 @@ export default class HistoryTable extends Component {
         </thead>
         {
           history.map((entry, index) => {
-            return <HistoryRow key={index} entry={entry} />;
+            return <HistoryRow key={index} bid={bid} entry={entry} />;
           })
         }
       </table>
