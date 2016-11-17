5c053caeb40d189b51bb06d5d447b4355128d6c3
Mathieu Leplatre <mathieu@mozilla.com>
2016-11-17 18:31:08 +0100

    Fix flow types

--- src/utils.js
+++ src/utils.js
@@ -22,11 +22,11 @@ export function isObject(thing: any): boolean {
 export function timeago(date: string | number): string {
   // Show relative time according to current timezone.
   const nowUTC = (new Date()).getTime();
+  const timestamp = parseInt(date, 10);
   // In our use case, we should never show relative time in the future.
   // For example, if local computer is late, the server timestamp will appear
   // to be in the future. Hence use "now" as a maximum.
-  const timestamp = nowUTC < date ? nowUTC : date;
-  return _timeago().format(timestamp);
+  return _timeago().format(Math.min(nowUTC, timestamp));
 }
 
 export function validJSON(string: string): boolean {
