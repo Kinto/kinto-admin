46017cfd1cc36b6aa25822f92d77e88e48321bd1
Nicolas Perriault <nicolas@perriault.net>
2016-11-18 11:46:40 +0100

    Typo

--- src/plugins/signoff/components.js
+++ src/plugins/signoff/components.js
@@ -110,7 +110,7 @@ export default class SignoffToolBar extends React.Component {
         {hasHistory ? null : (
           <div className="alert alert-warning">
             <p>
-              <b>Plugin which tracks history of changes is not enable on this server.</b>
+              <b>Plugin which tracks history of changes is not enabled on this server.</b>
               Please reach to the server administrator.
             </p>
           </div>
