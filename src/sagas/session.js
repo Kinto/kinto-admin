cb881ae28360924fd7d13e3660096c314fafcdf9
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-01-18 12:22:43 +0100

    Don't fail if buckets return a 403.

--- src/sagas/session.js
+++ src/sagas/session.js
@@ -74,7 +74,11 @@ export function* listBuckets(getState: GetStateFn, action: ActionType<typeof act
     // Notify they're received
     yield put(actions.serverInfoSuccess(serverInfo));
     // Retrieve and build the list of buckets
-    const {data} = yield call([client, client.listBuckets]);
+    try {
+      const {data} = yield call([client, client.listBuckets]);
+    } catch(error) {
+      const data = [];
+    }
     const responses = yield call([client, client.batch], (batch) => {
       for (const {id} of data) {
         batch.bucket(id).listCollections();
