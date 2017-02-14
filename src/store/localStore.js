26ec2d9fb6c0a60126c8879cf77c6d4f1c02b990
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-02-14 14:13:11 +0100

    Do not store passwords. Fixes #364

--- src/store/localStore.js
+++ src/store/localStore.js
@@ -7,7 +7,7 @@ const SESSION_KEY = "kinto-admin-session";
 
 
 export function loadHistory(): string[] {
-  const jsonHistory = localStorage.getItem(HISTORY_KEY);
+  const jsonHistory = sessionStorage.getItem(HISTORY_KEY);
   if (!jsonHistory) {
     return [];
   }
@@ -20,7 +20,7 @@ export function loadHistory(): string[] {
 
 export function saveHistory(history: string[]): string[] {
   try {
-    localStorage.setItem(HISTORY_KEY, JSON.stringify(history));
+    sessionStorage.setItem(HISTORY_KEY, JSON.stringify(history));
   } catch(err) {
     // Not much to do here, let's fail silently
   } finally {
@@ -34,14 +34,14 @@ export function clearHistory(): string[] {
 
 export function loadSession(): ?Object {
   try {
-    return JSON.parse(localStorage.getItem(SESSION_KEY) || "null");
+    return JSON.parse(sessionStorage.getItem(SESSION_KEY) || "null");
   } catch(err) {
     return null;
   }
 }
 
 export function saveSession(sessionState: SessionState): Promise<any> {
-  localStorage.setItem(SESSION_KEY, JSON.stringify({
+  sessionStorage.setItem(SESSION_KEY, JSON.stringify({
     ...sessionState,
     buckets: [],
   }));
@@ -49,6 +49,6 @@ export function saveSession(sessionState: SessionState): Promise<any> {
 }
 
 export function clearSession(): Promise<any> {
-  localStorage.removeItem(SESSION_KEY);
+  sessionStorage.removeItem(SESSION_KEY);
   return Promise.resolve();
 }
