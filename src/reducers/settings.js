39811b7ecdae62178e6a292c4fe9022c1b9efb40
Nicolas Perriault <nicolas@perriault.net>
2017-03-08 16:45:36 +0100

    Add portier as a default available auth method.

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -5,7 +5,7 @@ import type { SettingsState } from "../types";
 const INITIAL_STATE: SettingsState = {
   maxPerPage: 200,
   singleServer: null,
-  authMethods: ["basicauth", "fxa", "ldap", "anonymous"]
+  authMethods: ["basicauth", "fxa", "ldap", "portier", "anonymous"]
 };
 
 // This is basically a noop as we don't support settings update at runtime (yet).
