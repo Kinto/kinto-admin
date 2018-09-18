a35841e64233273c7880470741ead6147b39c3a4
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-09-18 14:07:04 -0400

    Try to fix babel-polyfill test failures

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ var version = require("./package.json").version;
 module.exports = {
   mode: "development",
   devtool: "#inline-source-map",
-  entry: ["babel-polyfill", "webpack-hot-middleware/client", "./index"],
+  entry: ["@babel/polyfill", "webpack-hot-middleware/client", "./index"],
   output: {
     path: path.join(__dirname, "build"),
     filename: "bundle.js",
