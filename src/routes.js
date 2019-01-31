2c8139cd0a193df70a844db95d9c518a790ca820
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-01-31 17:44:22 -0500

    Disable flow on the remaining line
    However, let's still try not to pass `routeUpdated` to the component.

--- src/routes.js
+++ src/routes.js
@@ -68,7 +68,43 @@ class ComponentWrapper extends PureComponent<ComponentWrapperProps> {
   };
 
   render() {
-    const { component: Component, ...props } = this.props;
+    const {
+      component: Component,
+      routeUpdated: _routeUpdated, // eslint-disable-line
+      ...props
+    } = this.props;
+    // Each component takes a different set of OwnProps, which leads
+    // to two problems with just calling the component with the props
+    // here.
+    //
+    // - OwnProps is an exact object, so we can't call a component
+    //   with "extra" props. We get all of the ContextRouter props and
+    //   there's no way to distinguish which of those is needed for
+    //   the component, so we have to pass all of them. It's true that
+    //   every OwnProps is essentially derived from the ContextRouter
+    //   type, so we *could* conceptually always use that, but that
+    //   means we don't get a clear understanding of which Route prop
+    //   each component uses. Besides, even if we do that...
+    //
+    // - We also customize the `match` type to better reflect the
+    //   params we expect to get from react-router based on the path.
+    //   react-router just gives us a string map, which isn't super
+    //   helpful. Collection components replace that with
+    //   CollectionRouteMatch etc. to assert that there should always
+    //   be e.g. a bid and a cid. Fixing the types here would require
+    //   adding some extra logic to ensure that the match we get is
+    //   what we expect (and if not.. throw?).
+    //
+    // But if we didn't wrap react-router, everything would be
+    // fine. Why is that? Of course, it's because react-router is
+    // written in JS and the typings in flow-typed aren't
+    // exact. `Route` is specified to take a component of any type,
+    // which gets unified with the OwnProps we actually take, even
+    // though `Route` always passes the complete set of router
+    // props. We can't reproduce this behavior in correctly-typed
+    // code, so let's not even try.
+
+    // $FlowFixMe: maybe wait until we get better types for react-router
     return <Component {...props} />;
   }
 }
