c45b6d553880ecd76504ff223b7b34d4a6a05135
Mathieu Leplatre <mathieu@mozilla.com>
2019-05-31 17:13:38 +0200

    Improve flow definition of kinto-http.js

--- interfaces/external-modules/kinto-http.d.js
+++ interfaces/external-modules/kinto-http.d.js
@@ -31,6 +31,7 @@ declare module "kinto-http" {
   declare type BatchResponse = {
     status: number,
     body: Object,
+    headers: Object,
   };
 
   declare type PermissionEntry = {
@@ -45,6 +46,21 @@ declare module "kinto-http" {
     sort?: string,
     limit?: number,
     since?: string,
+    at?: number,
+    pages?: number,
+    filters?: Object,
+  };
+
+  declare type ServerInfo = {
+    url: string,
+    project_name: string,
+    project_docs: string,
+    capabilities: Object,
+    user?: {
+      id: string,
+      principals: string[],
+      bucket?: string,
+    },
   };
 
   declare class KintoClient {
@@ -56,8 +72,8 @@ declare module "kinto-http" {
     bucket(bucketId: string): Bucket;
     createBucket(id: ?string, options?: Options): Promise<ObjectResponseBody<Resource>>;
     deleteBucket(id: string, options?: Options): Promise<ObjectResponseBody<Resource>>;
-    batch(): Promise<BatchResponse[]>;
-    fetchServerInfo(): Promise<Object>;
+    batch(batchFn: (batch: KintoClient) => void): Promise<BatchResponse[]>;
+    fetchServerInfo(): Promise<ServerInfo>;
     listBuckets(): Promise<ListResponseBody<Resource>>;
     listPermissions(): Promise<ListResponseBody<PermissionEntry>>;
   }
@@ -65,13 +81,15 @@ declare module "kinto-http" {
   declare class Bucket {
     constructor(): void;
     collection(collectionId: string): Collection;
-    setData(): Promise<*>;
+    getData(): Promise<Resource>;
+    setData(data: Object, options?: Options): Promise<ObjectResponseBody<Resource>>;
     setPermissions(permissions: Permissions): Promise<ObjectResponseBody<Resource>>;
     createCollection(id: ?string, options?: Options): Promise<ObjectResponseBody<Resource>>;
     deleteCollection(id: string, options?: Options): Promise<ObjectResponseBody<Resource>>;
     listCollections(options?: Options): Promise<ListResponseBody<Resource>>;
-    setData(data: Object, options?: Options): Promise<Object>,
     listHistory(options: Options): Promise<ListResponseBody<Object>>;
+    listGroups(): Promise<ListResponseBody<Resource>>;
+    getGroup(id: string, options?: Options): Promise<ListResponseBody<Resource>>;
     createGroup(id: ?string, members: string[], options?: Options): Promise<ObjectResponseBody<Resource>>;
     updateGroup(group: Object, options?: Options): Promise<ObjectResponseBody<Resource>>;
     deleteGroup(id: string, options?: Options): Promise<ObjectResponseBody<Resource>>;
@@ -79,17 +97,18 @@ declare module "kinto-http" {
 
   declare class Collection {
     constructor(): void;
-    getData(): Promise<Object>;
-    setData(data: Object, options?: Options): Promise<Object>;
+    getData(): Promise<Resource>;
+    setData(data: Object, options?: Options): Promise<ObjectResponseBody<Resource>>;
     setPermissions(permissions: Permissions): Promise<ObjectResponseBody<Resource>>;
     removeAttachment(): Promise<ObjectResponseBody<Resource>>;
     listRecords(options: ListRecordsOptions): Promise<ListResponseBody<Resource>>;
     getTotalRecords(options?: Object): Promise<number>;
     addAttachment(attachment: string, record: Object, options?: Options): Promise<ObjectResponseBody<Resource>>;
+    getRecord(rid: string, options?: Options): Promise<ObjectResponseBody<Resource>>;
     createRecord(record: Object, options?: Options): Promise<ObjectResponseBody<Resource>>;
     updateRecord(record: Object, options?: Options): Promise<ObjectResponseBody<Resource>>;
     deleteRecord(id: string, options?: Options): Promise<ObjectResponseBody<Resource>>;
-    batch(batchFn: (batch: Object) => void, options: Options): Promise<BatchResultAggregate>;
+    batch(batchFn: (batch: Collection) => void, options: Options): Promise<BatchResultAggregate>;
   }
 
   declare module.exports: typeof KintoClient
