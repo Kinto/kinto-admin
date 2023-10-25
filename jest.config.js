c634a6c68828dfe4c5ae7f7abf1a9045a747db79
Alex Cottner <acottner@mozilla.com>
2023-10-25 15:18:48 -0600

    fixed jest.config dup properties

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,9 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less)$": "<rootDir>/test/__mocks__/styleMock.js",
     "\\.svg": "<rootDir>/test/__mocks__/svgrMock.js",
+    "^nanoid$": "@rjsf/core/node_modules/nanoid/index.browser.cjs", // to avoid jest/es6 issue with @rjsf dependency
   },
   setupFilesAfterEnv: ["./test/setup-tests.js"],
   testEnvironment: "jest-environment-jsdom",
   testRegex: [".*_test.js", ".*_test.tsx", ".*_test.ts"],
-  moduleNameMapper: {
-    "^nanoid$": "@rjsf/core/node_modules/nanoid/index.browser.cjs" // to avoid jest/es6 issue with @rjsf dependency
-  }
 };
