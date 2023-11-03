03727967fc0bc55f6c86235350b343fb5925ee2a
Alex Cottner <acottner@mozilla.com>
2023-11-03 13:15:29 -0600

    Added several more unit tests.

--- test/components/signoff/components_test.js
+++ test/components/signoff/components_test.js
@@ -1,12 +1,8 @@
-import { expect } from "chai";
-
-import { createSandbox, createComponent } from "../../test_utils";
 import SignoffToolBar from "../../../src/components/signoff/SignoffToolBar";
 import * as React from "react";
+import { render, fireEvent } from "@testing-library/react";
 
 describe("SignoffToolBar component", () => {
-  let sandbox;
-
   const props = {
     sessionState: {
       permissions: [
@@ -48,27 +44,19 @@ describe("SignoffToolBar component", () => {
         },
       },
     },
-    requestReview: () => {},
-    confirmRequestReview: () => {},
-    approveChanges: () => {},
-    confirmRollbackChanges: () => {},
-    rollbackChanges: () => {},
-    declineChanges: () => {},
-    confirmDeclineChanges: () => {},
-    cancelPendingConfirm: () => {},
+    requestReview: jest.fn(),
+    confirmRequestReview: jest.fn(),
+    approveChanges: jest.fn(),
+    confirmRollbackChanges: jest.fn(),
+    rollbackChanges: jest.fn(),
+    declineChanges: jest.fn(),
+    confirmDeclineChanges: jest.fn(),
+    cancelPendingConfirm: jest.fn(),
   };
 
-  beforeEach(() => {
-    sandbox = createSandbox();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
-
   it("should not be rendered if current collection is not listed in resources", () => {
-    const node = createComponent(<SignoffToolBar {...props} signoff={{}} />);
-    expect(node).eql(null);
+    const node = render(<SignoffToolBar {...props} signoff={{}} />);
+    expect(node.container.innerHTML).toBe("");
   });
 
   it("should show the request review button if current user is editor", () => {
@@ -87,9 +75,53 @@ describe("SignoffToolBar component", () => {
         },
       },
     };
-    const node = createComponent(<SignoffToolBar {...propsOverride} />);
-    expect(node.querySelectorAll("button.request-review")).to.have.a.lengthOf(
-      1
-    );
+    const node = render(<SignoffToolBar {...propsOverride} />);
+    expect(node.queryAllByText("Request review...")).toHaveLength(1);
+  });
+
+  it("should show a spinner when the user clicks approve to prevent further changes", async () => {
+    // As the group is configured with a placeholder on the server, the group
+    // name is resolved with the current collection.
+    const propsOverride = {
+      ...props,
+      sessionState: {
+        ...props.sessionState,
+        serverInfo: {
+          ...props.sessionState.serverInfo,
+          user: {
+            id: "fxa:yo",
+            principals: [
+              "fxa:yo",
+              "/buckets/stage/groups/certs_editors",
+              "/buckets/stage/groups/reviewers",
+            ],
+          },
+        },
+      },
+      signoff: {
+        collectionsInfo: {
+          source: {
+            bid: "stage",
+            cid: "certs",
+            lastEditDate: 1524063083971,
+            changes: {
+              lastUpdated: 42,
+            },
+            status: "to-review",
+          },
+          destination: {
+            bid: "prod",
+            cid: "certs",
+          },
+        },
+      },
+      approveChanges: jest.fn(),
+    };
+    const node = render(<SignoffToolBar {...propsOverride} />);
+    expect(node.queryByTestId("spinner")).toBeNull();
+    expect(propsOverride.approveChanges).toHaveBeenCalledTimes(0);
+    fireEvent.click(await node.findByText("Approve"));
+    expect(node.queryByTestId("spinner")).toBeDefined();
+    expect(propsOverride.approveChanges).toHaveBeenCalledTimes(1);
   });
 });
