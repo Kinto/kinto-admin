1498f6461ef587a61a9b7ca56e5daa44c3e2a5e9
Alex Cottner <acottner@mozilla.com>
2024-05-28 11:35:14 -0600

    ran format

--- test/components/SessionInfoBar_test.tsx
+++ test/components/SessionInfoBar_test.tsx
@@ -1,11 +1,11 @@
-import { SessionInfoBar } from "@src/components/SessionInfoBar";
-import { screen, waitFor } from "@testing-library/react";
-import { renderWithProvider } from "@test/testUtils";
 import { getClient, setClient } from "@src/client";
+import { SessionInfoBar } from "@src/components/SessionInfoBar";
+import { renderWithProvider } from "@test/testUtils";
+import { screen, waitFor } from "@testing-library/react";
 
 describe("SessionInfoBar component", () => {
   const client = {
-    execute: vi.fn()
+    execute: vi.fn(),
   };
   const healthyStr = "Server heartbeat status is healthy";
   const unhealthyStr = "Server heartbeat status IS NOT healthy";
@@ -18,27 +18,27 @@ describe("SessionInfoBar component", () => {
     vi.resetAllMocks();
   });
 
-  it("Should show green by default", async() => {
+  it("Should show green by default", async () => {
     client.execute.mockResolvedValue({});
     renderWithProvider(<SessionInfoBar />);
     await waitFor(() => new Promise(resolve => setTimeout(resolve, 100))); // debounce wait
     expect(screen.getByTitle(healthyStr)).toBeDefined();
   });
 
-  it("Should show green when heartbeat returns all true checks", async() => {
+  it("Should show green when heartbeat returns all true checks", async () => {
     client.execute.mockResolvedValue({
       foo: true,
-      bar: true
+      bar: true,
     });
     renderWithProvider(<SessionInfoBar />);
     await waitFor(() => new Promise(resolve => setTimeout(resolve, 100))); // debounce wait
     expect(screen.getByTitle(healthyStr)).toBeDefined();
   });
 
-  it("Should show red x when heartbeat returns any false checks", async() => {
+  it("Should show red x when heartbeat returns any false checks", async () => {
     client.execute.mockResolvedValue({
       foo: false,
-      bar: true
+      bar: true,
     });
     renderWithProvider(<SessionInfoBar />);
     await waitFor(() => new Promise(resolve => setTimeout(resolve, 100))); // debounce wait
@@ -46,10 +46,10 @@ describe("SessionInfoBar component", () => {
     expect(screen.getByTitle(unhealthyStr)).toBeDefined();
   });
 
-  it("Should show red x when heartbeat check throws an error", async() => {
+  it("Should show red x when heartbeat check throws an error", async () => {
     client.execute.mockImplementation(() => {
       throw new Error("Test error");
-    })
+    });
     renderWithProvider(<SessionInfoBar />);
     await waitFor(() => new Promise(resolve => setTimeout(resolve, 100))); // debounce wait
     expect(screen.getByTitle(unhealthyStr)).toBeDefined();
