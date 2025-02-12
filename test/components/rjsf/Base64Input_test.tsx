7c3e14b109c0aba90e574cd9abf0ecb3ff632e48
Alex Cottner <acottner@mozilla.com>
2025-02-12 15:04:48 -0700

    outlining unit tests

---
+++ test/components/rjsf/Base64Input_test.tsx
@@ -0,0 +1,25 @@
+import Base64Input from "@src/components/rjsf/Base64Input";
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react"
+
+describe("Base64Input rjsf component", () => {
+  it("Should render empty when no value is provided", async() => {
+
+  });
+
+  it("Should render an image when one the existing value is an image", async() => {
+
+  });
+
+  it("Should render a download link when the existing value is not an image", async() => {
+
+  });
+
+  it("Should update the value when a new file is uploaded", async() => {
+
+  });
+
+  it("Should have a validation error when the uploaded file is too large", async() => {
+
+  });
+});
