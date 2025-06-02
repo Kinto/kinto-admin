24963843e13d8567014e227fd91908a82348f56d
Nitin Tejuja <95347924+nitintecg@users.noreply.github.com>
2025-06-02 20:36:49 +0530

    Remote-Setttings Issue 821 - Replacing redux with hooks for heartbeat (#3473)
    https://github.com/mozilla/remote-settings/issues/821

---
+++ src/hooks/heartbeat.ts
@@ -0,0 +1,45 @@
+import { getClient } from "@src/client";
+import { HeartbeatState } from "@src/types";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+export function useHeartbeat(): HeartbeatState {
+  const [val, setVal] = useState<HeartbeatState>({
+    success: true,
+    response: {},
+  });
+
+  useEffect(() => {
+    queryHeartbeat(setVal);
+  }, []);
+
+  return val;
+}
+
+async function queryHeartbeat(
+  setVal: Dispatch<SetStateAction<HeartbeatState>>
+) {
+  const client = getClient();
+  try {
+    const response: Record<string, any> = await client.execute({
+      path: "/__heartbeat__",
+      headers: undefined,
+    });
+    const success = !Object.values(response).some(value => value === false);
+    setVal({
+      success,
+      response,
+    });
+  } catch (ex) {
+    setVal({
+      success: false,
+      details: ex,
+    });
+    console.log({
+      success: false,
+      details: ex,
+    });
+  }
+  setTimeout(() => {
+    queryHeartbeat(setVal);
+  }, 60000);
+}
