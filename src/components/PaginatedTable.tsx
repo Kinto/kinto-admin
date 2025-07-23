6f9d52e0dd4a16a4b11db50fee8474b6b11006fd
Mathieu Leplatre <mathieu@mozilla.com>
2025-07-23 08:37:12 +0200

    Fix #3061: Adjust table columns sizes (#3584)
    * Fix #3061: Adjust table columns sizes
    
    * Center actions block on records list
    
    * Link to records from buckets collections lists

--- src/components/PaginatedTable.tsx
+++ src/components/PaginatedTable.tsx
@@ -19,38 +19,40 @@ export default function PaginatedTable({
   listNextPage,
 }: Props) {
   return (
-    <table
-      className="table table-striped table-bordered record-list"
-      data-testid="paginatedTable"
-    >
-      {thead}
-      {tbody}
-      {hasNextPage && (
-        <tfoot>
-          <tr>
-            <td colSpan={colSpan} className="load-more text-center">
-              {!dataLoaded ? (
-                <Spinner />
-              ) : (
-                <a
-                  href="."
-                  key="__3"
-                  onClick={event => {
-                    event.preventDefault();
-                    // FIXME: we should always have listNextPage if
-                    // we have hasNextPage
-                    if (listNextPage) {
-                      listNextPage();
-                    }
-                  }}
-                >
-                  Load more
-                </a>
-              )}
-            </td>
-          </tr>
-        </tfoot>
-      )}
-    </table>
+    <div className="table-wrapper">
+      <table
+        className="table table-striped table-bordered record-list"
+        data-testid="paginatedTable"
+      >
+        {thead}
+        {tbody}
+        {hasNextPage && (
+          <tfoot>
+            <tr>
+              <td colSpan={colSpan} className="load-more text-center">
+                {!dataLoaded ? (
+                  <Spinner />
+                ) : (
+                  <a
+                    href="."
+                    key="__3"
+                    onClick={event => {
+                      event.preventDefault();
+                      // FIXME: we should always have listNextPage if
+                      // we have hasNextPage
+                      if (listNextPage) {
+                        listNextPage();
+                      }
+                    }}
+                  >
+                    Load more
+                  </a>
+                )}
+              </td>
+            </tr>
+          </tfoot>
+        )}
+      </table>
+    </div>
   );
 }
