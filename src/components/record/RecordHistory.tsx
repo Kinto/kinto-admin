a1fdae4aaf54f28bbdb964c13538258d16d822c6
Mathieu Leplatre <mathieu@mozilla.com>
2025-08-11 16:12:44 +0200

    Leverage in record history

--- src/components/record/RecordHistory.tsx
+++ src/components/record/RecordHistory.tsx
@@ -1,12 +1,27 @@
 import RecordTabs from "./RecordTabs";
 import HistoryTable from "@src/components/HistoryTable";
+import { useShowNonHumans, useShowSignerPlugin } from "@src/hooks/preferences";
 import { useRecordHistory } from "@src/hooks/record";
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams } from "react-router";
 
 export default function RecordHistory() {
   const { bid, cid, rid } = useParams();
-  const history = useRecordHistory(bid, cid, rid);
+
+  // Restore preferences
+  const [showSignerPlugin, setShowSignerPlugin] = useShowSignerPlugin(true);
+  const [showNonHumans, setShowNonHumans] = useShowNonHumans(true);
+
+  // Create filters object from current state
+  const filters = useMemo(
+    () => ({
+      show_signer_plugin: showSignerPlugin,
+      show_non_humans: showNonHumans,
+    }),
+    [showSignerPlugin, showNonHumans]
+  );
+
+  const history = useRecordHistory(bid, cid, rid, filters);
 
   return (
     <div>
@@ -23,6 +38,11 @@ export default function RecordHistory() {
           history={history.data ?? []}
           hasNextHistory={history.hasNextPage}
           listNextHistory={history.next}
+          showNonHumans={showNonHumans}
+          showSignerPlugin={showSignerPlugin}
+          // Persist filter changes and reload table (memo dependencies)
+          onShowSignerPluginChange={setShowSignerPlugin}
+          onShowNonHumansChange={setShowNonHumans}
         />
       </RecordTabs>
     </div>
