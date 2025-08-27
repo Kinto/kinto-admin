7a4305c1a2eda00be4c0a2493f72c8b163c6a55e
Mathieu Leplatre <mathieu@mozilla.com>
2025-08-27 10:34:38 +0200

    mozilla/remote-settings#995: allow to copy timestamp values from lists (#3627)
    * Add copy button to timestamps
    
    * Move HumanDate out of signoff/
    
    * Leverage HumanDate everywhere

--- src/components/signoff/Review.tsx
+++ src/components/signoff/Review.tsx
@@ -1,7 +1,7 @@
 import AdminLink from "../AdminLink";
 import { Comment } from "./Comment";
-import HumanDate from "./HumanDate";
 import { ProgressStep } from "./ProgressBar";
+import HumanDate from "@src/components/HumanDate";
 import { useSimpleReview } from "@src/hooks/preferences";
 import type {
   ChangesList,
