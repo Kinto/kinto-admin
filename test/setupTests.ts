6290ad906dfd30998a7d42a2e2bd79394f48d186
Alex Cottner <acottner@mozilla.com>
2025-11-26 05:36:35 -1000

    Working around odd 'module not found' error with @rjsf/react-bootstrap

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -3,4 +3,11 @@ import { vi } from "vitest";
 
 vi.mock("kinto");
 
+// @rjsf/react-bootstrap has some odd vitest compat error
+vi.mock("@rjsf/react-bootstrap", () => {
+  return {
+    Theme: {}
+  };
+});
+
 Object.defineProperty(window, "scrollTo", { value: vi.fn(), writable: true });
